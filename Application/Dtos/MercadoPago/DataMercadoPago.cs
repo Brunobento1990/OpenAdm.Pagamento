02d111607aa0abdacc7f4f705fcb1a5c9e45903a
Bruno <brunobentocaina@gmail.com>
2024-06-16 19:33:19 -0300

    fix

--- Application/Dtos/MercadoPago/DataMercadoPago.cs
+++ Application/Dtos/MercadoPago/DataMercadoPago.cs
@@ -1,6 +1,9 @@
-﻿namespace Application.Dtos.MercadoPago;
+﻿using System.Text.Json.Serialization;
+
+namespace Application.Dtos.MercadoPago;
 
 public class DataMercadoPago
 {
+    [JsonPropertyName("id")]
     public long Id { get; set; }
 }
--- Application/Dtos/MercadoPago/MercadoPagoWebHook.cs
+++ Application/Dtos/MercadoPago/MercadoPagoWebHook.cs
@@ -1,8 +1,13 @@
-﻿namespace Application.Dtos.MercadoPago;
+﻿using System.Text.Json.Serialization;
+
+namespace Application.Dtos.MercadoPago;
 
 public class MercadoPagoWebHook
 {
+    [JsonPropertyName("action")]
     public string? Action { get; set; } 
+    [JsonPropertyName("id")]
     public string? Id { get; set; }
+    [JsonPropertyName("data")]
     public DataMercadoPago? Data { get; set; }
 }
